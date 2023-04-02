b89818fe9fb1976d873910a2cf729fbb9aec446b
Sumuk Shashidhar <sumukuuu@gmail.com>
2023-04-02 12:36:22 -0500

    feat: add preliminary test script

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from modules.load_vars import load_env_file
+from modules.get_files import get_jpg_files
+# define the env file path
+env_file = '.ENV'
+
+# load the env file
+env = load_env_file(env_file)
+
+# get the list of jpg files
+image_list = get_jpg_files(env['FILEPATH'])
+
+# print a sample
+print(image_list[0])
